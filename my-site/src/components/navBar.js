ca840736e77cc9961cffbd013cbc872772c425fc
triebelscj <cjtriebels208@gmail.com>
2021-02-25 08:54:56 -0800

    first commit

---
+++ my-site/src/components/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function navBar() {
+    return (
+        <nav className="navFlex">
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Portfolio</h1>
+            <h1>Contact</h1>
+        </nav>
+    )
+}
+
+export default navBar
